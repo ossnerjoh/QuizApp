1165ae9c10d65cf7ecb41148962acb43d394f775
ossnerjoh <74686380+ossnerjoh@users.noreply.github.com>
2023-02-14 18:55:27 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App card">
+      <div className="card-header">My App</div>
+      <div className="card-body">
+        <h5>Überschrift</h5>
+        <p className="card-text">Dies ist ein Absatz </p>
+      </div>
+    </div>
+  );
+}
