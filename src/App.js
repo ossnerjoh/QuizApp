a005d3305d07bbe7d7daacc2e70a575135744d77
ossnerjoh <74686380+ossnerjoh@users.noreply.github.com>
2023-02-15 19:28:30 +0100

    Quiz and Results Logic finished next: ResultsDisplay Component and AddQuestion View

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import "./styles.css";
-import {quizItems} from "./data/quizData";
+import { quizItems } from "./data/quizData";
 import QuizDisplay from "./components/QuizDisplay";
 import { useState } from "react";
 
 export default function App() {
-  const [quizNr, setQuizNr]= useState(0);
-  console.log(quizItems)
-  return (
-    <QuizDisplay quizItem={quizItems[quizNr]}/>
+  const [hasQuizesLeft, setHasQuizesLeft] = useState(true);
+  const [quizCounter, setQuizCounter] = useState(0);
+  const [currentQuizItem, setCurrentQuizItem] = useState(quizItems[0]);
+  const [answers, setAnswers] = useState([]);
+
+  function setNextQuestion(selectedAnswer) {
+    setAnswers([...answers, selectedAnswer]);
+    if (quizCounter === quizItems.length - 1) {
+      setHasQuizesLeft(false);
+    } else {
+      const newQuizCounter = quizCounter + 1;
+      setQuizCounter(newQuizCounter);
+      setCurrentQuizItem(quizItems[newQuizCounter]);
+    }
+  }
+  return hasQuizesLeft ? (
+    <QuizDisplay quizItem={currentQuizItem} setNextQuestion={setNextQuestion} />
+  ) : (
+    <div>
+      You answered{" "}
+      {
+        answers.filter((answ, index) => {
+          return answ === quizItems[index].correct;
+        }, 0).length
+      }{" "}
+      of {quizItems.length} Answers correct
+    </div>
   );
 }
--- src/components/QuizDisplay.jsx
+++ src/components/QuizDisplay.jsx
@@ -1,33 +1,84 @@
-import React from 'react'
+import React, { useState } from "react";
 
-function QuizDisplay({quizItem}) {
+function QuizDisplay({ quizItem, setNextQuestion }) {
+  const [selectedAnswer, setSelectedAnswer] = useState("");
+
+  function onChangeValue(event) {
+    setSelectedAnswer(event.target.value);
+  }
+
   return (
-    <div className="quiz-container" id="quiz">
+    <div className="quiz-container">
       <div className="quiz-header">
         <h2 id="question">{quizItem.question}</h2>
         <ul>
           <li>
-            <input type="radio" name="answer" value="a" id="a" className="answer" />
-            <label htmlFor="a" id="a_text">{quizItem.a}</label>
+            <input
+              type="radio"
+              name="answer"
+              value="a"
+              checked={selectedAnswer === "a"}
+              onChange={onChangeValue}
+              id="a"
+              className="answer"
+            />
+            <label htmlFor="a" id="a_text">
+              {quizItem.a}
+            </label>
           </li>
           <li>
-            <input type="radio" name="answer" value="b" id="b" className="answer" />
-            <label htmlFor="b" id="b_text">{quizItem.b}</label>
+            <input
+              type="radio"
+              name="answer"
+              value="b"
+              checked={selectedAnswer === "b"}
+              onChange={onChangeValue}
+              id="b"
+              className="answer"
+            />
+            <label htmlFor="b" id="b_text">
+              {quizItem.b}
+            </label>
           </li>
           <li>
-            <input type="radio" name="answer" value="c" id="c" className="answer" />
-            <label htmlFor="c" id="c_text">{quizItem.c}</label>
+            <input
+              type="radio"
+              name="answer"
+              value="c"
+              checked={selectedAnswer === "c"}
+              onChange={onChangeValue}
+              id="c"
+              className="answer"
+            />
+            <label htmlFor="c" id="c_text">
+              {quizItem.c}
+            </label>
           </li>
           <li>
-            <input type="radio" name="answer" value="d" id="d" className="answer" />
-            <label htmlFor="d" id="d_text">{quizItem.d}</label>
+            <input
+              type="radio"
+              name="answer"
+              value="d"
+              checked={selectedAnswer === "d"}
+              onChange={onChangeValue}
+              id="d"
+              className="answer"
+            />
+            <label htmlFor="d" id="d_text">
+              {quizItem.d}
+            </label>
           </li>
         </ul>
       </div>
-      <button id="submit">Submit</button>
+      <button
+        onClick={() => {
+          setNextQuestion(selectedAnswer);
+        }}
+      >
+        Next
+      </button>
     </div>
-
-  )
+  );
 }
 
-export default QuizDisplay
+export default QuizDisplay;
